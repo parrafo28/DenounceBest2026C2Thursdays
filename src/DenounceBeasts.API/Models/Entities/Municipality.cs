638c999d006173dc3a00e9e361964257daec6bc4
Starling Germosen <sgermosen@outlook.com>
2026-05-28 20:38:37 -0400

    Adding the municipality controller and entity

---
+++ src/DenounceBeasts.API/Models/Entities/Municipality.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DenounceBeasts.API.Models.Entities
+{
+    public class Municipality
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string? PostalCode { get; set; }
+        public bool IsActive { get; set; } = true;
+
+    }
+}
